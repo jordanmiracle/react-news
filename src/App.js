6562c501bbddb8918fcc71234d0aeb8070f2b330
Jordan Miracle <jordanmiracle@protonmail.com>
2022-01-04 03:07:02 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { getArticles } from "./api";
+import { Container, Header } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    articles: [],
+    apiError: "",
+  };
+
+    render() {
+        const { articles, apiError } = this.state;
+        return (
+            <Container>
+                <Header as="h2" style={{ textAlign: "center", margin: 20 }}>
+                    Bitcoin articles
+                </Header>
+                {articles.length > 0 && <ArticleList articles={articles} />}
+                {apiError && <p>Could not fetch any articles. Please try again.</p>}
+            </Container>
+        );
+    }
 }
 
 export default App;
